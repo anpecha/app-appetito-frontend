3818472b14d614255d89758660601354e95b3b01
anpecha <anpecha@users.noreply.github.com>
2026-07-01 14:03:16 -0300

    fix: add dynamic rendering to cashier page to fix SSG build

--- src/app/admin/cashier/page.tsx
+++ src/app/admin/cashier/page.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import React, { useEffect, useState, useMemo, useCallback } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import {
